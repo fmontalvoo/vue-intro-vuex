7982dd4c0900512652c7e03d34f23a2fc9e99346
Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>
2022-11-29 23:04:58 -0500

    Se lee el state del store.
    Signed-off-by: Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Counter Vuex</h1>
+    <p>Acceso directo: {{ $store.state.count }}</p>
+    <p>Map state: {{ count }}</p>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    computed: {
+        ...mapState(['count'])
+    }
+}
+</script>
+
+<style>
+
+</style>
