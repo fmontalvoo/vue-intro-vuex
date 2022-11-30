ef33d2bcf775af56c2548a6b69b259b1d79a2427
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-30 08:45:34 -0500

    Mutations
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/components/CounterComponent.vue
+++ src/components/CounterComponent.vue
@@ -2,6 +2,14 @@
     <h1>Counter Vuex</h1>
     <p>Acceso directo: {{ $store.state.count }}</p>
     <p>Map state: {{ count }}</p>
+    <div>
+        <button class="minus" @click="substract">-</button>
+        <button class="plus" @click="add">+</button>
+    </div>
+    <div>
+        <button class="minus" @click="substractNumber">-</button>
+        <button class="plus" @click="addNumber">+</button>
+    </div>
 </template>
 
 <script>
@@ -10,10 +18,44 @@ import { mapState } from 'vuex'
 export default {
     computed: {
         ...mapState(['count'])
+    },
+    methods: {
+        add() {
+            this.$store.commit('add');
+        },
+        substract() {
+            this.$store.commit('substract');
+        },
+        addNumber() {
+            this.$store.commit('addNumber', 7);
+        },
+        substractNumber() {
+            this.$store.commit('substractNumber', 7);
+        },
     }
 }
 </script>
 
-<style>
+<style scoped>
+.minus {
+    margin: 5px;
+    width: 30px;
+    height: 30px;
+    border-width: 1;
+    border-radius: 5px;
+    color: aliceblue;
+    border-color: crimson;
+    background-color: crimson;
+}
 
+.plus {
+    margin: 5px;
+    width: 30px;
+    height: 30px;
+    border-width: 1;
+    border-radius: 5px;
+    color: aliceblue;
+    border-color: green;
+    background-color: green;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,19 @@ import { createStore } from 'vuex'
 export default createStore({
     state: {
         count: 0
+    },
+    mutations: {
+        add(state) {
+            state.count += 1
+        },
+        substract(state) {
+            state.count -= 1
+        },
+        addNumber(state, value) {
+            state.count += value
+        },
+        substractNumber(state, value) {
+            state.count -= value
+        },
     }
 })
